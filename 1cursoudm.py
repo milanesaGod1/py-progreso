3c34e5056e7116d1a6ee24e9d7de12da8b51f844
MRmarcorojas <125605449+milanesaGod1@users.noreply.github.com>
2023-03-06 18:48:59 -0300

    Add files via upload

---
+++ 1cursoudm.py
@@ -0,0 +1,13 @@
+# ejercicio de python basico
+
+name = "Pyratilla"
+age = 17
+pet = True
+pet_name = "Pyo"
+ocupation = "Pirata"  # Y en cuanto tenga tripulación, seré capitán
+coins = 0
+
+print(name, age, pet, pet_name, ocupation, coins)
+
+coins += 100
+print(coins)
